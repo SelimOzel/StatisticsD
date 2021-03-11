fd7be1b36f61e84239d5b30d2fd361ff614ef585
Selim Ozel <sozel@wpi.edu>
2021-03-11 22:30:44 +0100

    I like visuals more with these values.

--- source/tests/app.d
+++ source/tests/app.d
@@ -8,8 +8,8 @@ void main()
 	double[] time_series_2;
 
 	// Stationary process
-	time_series_1 = generate_autoregressive_process(1.0, 0.5, 1.0, 1000);
-	time_series_2 = generate_autoregressive_process(1.0, 0.5, 1.0, 1000);
+	time_series_1 = generate_autoregressive_process(1.0, 0.9, 1.0, 1000);
+	time_series_2 = generate_autoregressive_process(1.0, 0.9, 1.0, 1000);
 	writeln(compute_mean(time_series_1));
 	writeln(compute_std(time_series_1));
 	writeln(compute_correlation(time_series_1, time_series_2));
