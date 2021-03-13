fbe16e8f21c87aa822ebcc0dfb9afdfe7af59562
Selim Ozel <sozel@wpi.edu>
2021-03-13 02:27:24 +0100

    OLS finished and plots the estimated value on top of random walk.

--- source/test_ols/app.d
+++ source/test_ols/app.d
@@ -6,13 +6,19 @@ import plt = matplotlibd.pyplot;
 void main()
 {
 	double[] time_series;
+	double[] time_series_estimated;
+	double[2] ols;
 
 	int n = 1000;
 	double[] t;
 	for (int i = 0; i<n; i++) {t ~= i;}
 
 	time_series = generate_autoregressive_process(0.1, 1.0, 1.0, n);
+	ols = compute_ordinary_least_squares(t, time_series);
+	for (int i = 0; i<n; i++) {time_series_estimated ~= ols[0] + ols[1]*i;}
+
 	plt.plot(t, time_series, "o", ["markersize": 0.5]);
+	plt.plot(t, time_series_estimated, "r-");
 	plt.legend();
 	plt.grid();
 	plt.savefig("Random Process.png");
